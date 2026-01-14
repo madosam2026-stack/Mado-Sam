d9a7a01efb52d046e846d4ad152d6c346bfbadb7
madosam2026-stack <Mado.sam2026@gmail.com>
2026-01-14 20:45:38 +0100

    merge remote main3

--- src/redux/features/quickView-slice.ts
+++ src/redux/features/quickView-slice.ts
@@ -2,38 +2,23 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Product } from "@/types/product";
 
 type InitialState = {
-  value: Product;
+  value: Product | null;
 };
 
-const initialState = {
-  value: {
-    title: "",
-    reviews: 0,
-    price: 0,
-    discountedPrice: 0,
-    img: "",
-    id: 0,
-    images: [],
-    imgs: { thumbnails: [], previews: [] },
-  } as Product,
-} as InitialState;
+const initialState: InitialState = {
+  value: null,
+};
 
 export const quickView = createSlice({
   name: "quickView",
   initialState,
   reducers: {
-    updateQuickView: (_, action) => {
-      return {
-        value: {
-          ...action.payload,
-        },
-      };
+    updateQuickView: (state, action: PayloadAction<Product>) => {
+      state.value = action.payload;
     },
 
-    resetQuickView: () => {
-      return {
-        value: initialState.value,
-      };
+    resetQuickView: (state) => {
+      state.value = null;
     },
   },
 });
