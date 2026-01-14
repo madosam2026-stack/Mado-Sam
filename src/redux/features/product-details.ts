39b3945a7312a3333bb60d787b89fe41b7368162
madosam2026-stack <Mado.sam2026@gmail.com>
2026-01-14 20:42:26 +0100

    merge remote main2

--- src/redux/features/product-details.ts
+++ src/redux/features/product-details.ts
@@ -1,38 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Product } from "@/types/product";
 
 type InitialState = {
-  value: Product;
+  value: Product | null;
 };
 
-const initialState = {
-  value: {
-    title: "",
-    category: "",
-    description: "",
-    reviews: 0,
-    price: 0,
-    discountedPrice: 0,
-    img: "",
-    images: [],
-    id: 0,
-    imgs: { thumbnails: [], previews: [] },
-  },
-} as InitialState;
+const initialState: InitialState = {
+  value: null,
+};
 
 export const productDetails = createSlice({
   name: "productDetails",
   initialState,
   reducers: {
-    updateproductDetails: (_, action) => {
-      return {
-        value: {
-          ...action.payload,
-        },
-      };
+    updateproductDetails: (state, action: PayloadAction<Product>) => {
+      state.value = action.payload;
     },
   },
 });
 
 export const { updateproductDetails } = productDetails.actions;
 export default productDetails.reducer;
+
